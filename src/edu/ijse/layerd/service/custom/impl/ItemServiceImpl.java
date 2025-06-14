2eaadde6fccbe27eadd5c60bd9676b96ce61f902
Anjana Thrishakya <anjanathrishakya@gmail.com>
2025-06-14 11:50:22 +0530

    ItemService Implementation

--- src/edu/ijse/layerd/service/custom/impl/ItemServiceImpl.java
+++ src/edu/ijse/layerd/service/custom/impl/ItemServiceImpl.java
@@ -5,6 +5,7 @@
 package edu.ijse.layerd.service.custom.impl;
 
 import edu.ijse.layerd.dto.ItemDto;
+import edu.ijse.layerd.entity.ItemEntity;
 import edu.ijse.layerd.service.custom.ItemService;
 import java.util.ArrayList;
 
@@ -16,27 +17,60 @@ public class ItemServiceImpl implements ItemService{
 
     @Override
     public String saveItem(ItemDto itemDto) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        ItemEntity itemEntity = new ItemEntity(itemDto.getCode(),
+                itemDto.getDesc(), itemDto.getPack(),
+                itemDto.getUnitPrice(), itemDto.getQoh());
+        
+        boolean isSaved = true;
+        return isSaved ? "Success" : "Fail";
     }
 
     @Override
     public String updateItem(ItemDto itemDto) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        ItemEntity itemEntity = new ItemEntity(itemDto.getCode(),
+                itemDto.getDesc(), itemDto.getPack(),
+                itemDto.getUnitPrice(), itemDto.getQoh());
+        
+        boolean isUpdate = true;
+        return isUpdate ? "Success" : "Fail";
     }
 
     @Override
     public String deleteItem(String itemCode) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
+        boolean isDeleted = true;
+        return  isDeleted ? "Success" : "Fail";
     }
 
     @Override
     public ItemDto searchItem(String itemCode) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        ItemEntity itemEntity = null;
+        if(itemEntity != null){
+            return new ItemDto(itemEntity.getCode(), 
+                    itemEntity.getDesc(), 
+                    itemEntity.getPack(),
+                    itemEntity.getUnitPrice(),
+                    itemEntity.getQoh());
+        }
+        
+        return null;
     }
 
     @Override
     public ArrayList<ItemDto> getAllItem() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        ArrayList<ItemEntity> itemEntitys = null;
+        ArrayList<ItemDto> itemDtos = new ArrayList<>();
+        if(itemEntitys != null){
+            for (ItemEntity itemEntity : itemEntitys) {
+                itemDtos.add(new ItemDto(itemEntity.getCode(), 
+                    itemEntity.getDesc(), 
+                    itemEntity.getPack(),
+                    itemEntity.getUnitPrice(),
+                    itemEntity.getQoh()));
+            }
+        }
+        
+        return itemDtos;
     }
     
 }
